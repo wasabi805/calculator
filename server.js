f2a5369e263dced192ecc30dff8e166115e30cba
‘Tim <timothy.j.ocampo@gmail.com>
2019-02-13 12:55:37 -0800

    removed the process check.

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname + '/public/views/calculator_jQuery.html'));
 });
 
-
 const port = process.env.PORT|| 5000;
 app.listen(port, ()=>{
     console.log( `Server running on port ${port}` );
